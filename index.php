c11c8c7e166466d84dc7293c142749f6506bcce8
Hari haran <hariharan.rhce@gmail.com>
2019-07-18 14:43:06 +0530

    sample

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
-<?php
-echo "file completed "
-?>
+<?php  
+$alpha = range('A', 'Z');  
+for($i=0; $i<5; $i++){   
+  for($j=5; $j>$i; $j--){  
+    echo $alpha[$i];  
+    }  
+    echo "<br>";  
+}  
+?>  
